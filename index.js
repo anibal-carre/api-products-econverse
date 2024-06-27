beb55e26c14c8b33893843a23b12a99d02a38ad6
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2024-06-27 14:54:07 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import { products } from "./data/products.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
+
+app.get("/api/econverse/products", (req, res) => {
+  try {
+    return res.json(products);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.listen(3000);
+console.log(`Server on port ${3000}`);
